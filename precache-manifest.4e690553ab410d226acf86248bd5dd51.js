e37a3588600c88ddd56b94c9c260e0974bff302f
Sandip Savani <savani045@gmail.com>
2020-09-14 10:16:43 +0530

    Updates

---
+++ precache-manifest.4e690553ab410d226acf86248bd5dd51.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1cca5e3650cbbcb14733157c588f53b",
+    "url": "/sandipsavani/ecom-app.git/index.html"
+  },
+  {
+    "revision": "52e321694a9586b8284a",
+    "url": "/sandipsavani/ecom-app.git/static/css/main.6c79112f.chunk.css"
+  },
+  {
+    "revision": "16b17c9430337adf77ef",
+    "url": "/sandipsavani/ecom-app.git/static/js/2.3e871266.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/sandipsavani/ecom-app.git/static/js/2.3e871266.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "52e321694a9586b8284a",
+    "url": "/sandipsavani/ecom-app.git/static/js/main.2f2e8d8e.chunk.js"
+  },
+  {
+    "revision": "8811cfa1e35d841907c6",
+    "url": "/sandipsavani/ecom-app.git/static/js/runtime-main.ce5c5897.js"
+  }
+]);
